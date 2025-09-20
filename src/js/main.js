e22ebc113781a92c432eacc77482788ad841d55e
Brian <bj.johnson@oracle.com>
2025-09-19 22:20:46 -0400

    changed the embedded SVGs to reference svgs to make the .html file size smaller

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,10 @@
 import projectCardClickHandler from './projectCards.js';
 import setupNavToggle from './toggleNav.js';
 
-// We are now using a simple, hardcoded path to the SVG file
-// because it's in the 'public' folder and will be copied as-is.
-const iconMapURL = '/bj-icon-map.svg';
-
 document.addEventListener('DOMContentLoaded', function () {
 
   //**EVENT LISTENERS **/
   projectCardClickHandler();
   setupNavToggle();
 
-  // The rest of this code remains the same
-  const iconUses = document.querySelectorAll('a.banner__icons svg use');
-
-  iconUses.forEach(use => {
-    const iconId = use.getAttribute('xlink:href');
-    use.setAttribute('xlink:href', `${iconMapURL}${iconId}`);
-  });
 });
